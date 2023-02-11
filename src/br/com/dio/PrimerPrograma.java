162194fb6e9247a02619ea331b40a8c9dc3170d5
wjunes <willans@tuyaymia.com>
2023-02-11 17:22:30 -0300

    First Commit

---
+++ src/br/com/dio/PrimerPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import model.Gato;
+
+public class PrimerPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+      /*  int a = 5;
+        int b = 7;
+        System.out.println("HELLO WORLD " + (a + b));*/
+    }
+}
